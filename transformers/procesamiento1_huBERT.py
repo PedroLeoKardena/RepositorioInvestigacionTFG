dc8a95342a806713ae546d733e182640f32a4183
PedroLeoKardena <pedrojleom@gmail.com>
2026-06-03 11:15:12 +0200

    Tuneo hubert

--- transformers/procesamiento1_huBERT.py
+++ transformers/procesamiento1_huBERT.py
@@ -1,12 +1,12 @@
 import os
 from tune_base_pipeline import BaseTransformerPipeline, HubertMultiTask
 
-LR = 5e-5
+LR = 3e-5
 BATCH_SIZE = 4
 GRAD_STEPS = 4
-EPOCHS = 8
-WEIGHT_DECAY = 0.01
-WARMUP_STEPS = 100
+EPOCHS = 10
+WEIGHT_DECAY = 0.05
+WARMUP_STEPS = 250
 
 class HubertBaselinePipeline(BaseTransformerPipeline):
     @property
